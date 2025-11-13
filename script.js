582821538409258e11c00a69add6e7c031bc7329
mannatgupta146 <mannatgupta146@gmail.com>
2025-11-14 02:04:39 +0530

    done

---
+++ script.js
@@ -0,0 +1,27 @@
+let h1 = document.querySelector("h1")
+let inc = document.getElementById("plus")
+let dec = document.getElementById("minus")
+let reset = document.getElementById("set")
+
+let a = 0
+
+inc.addEventListener("click", function(){
+    inc.style.backgroundColor = "blue"
+    setTimeout(() => inc.style.backgroundColor = "", 500);
+    a++
+    h1.innerHTML = a
+})
+
+dec.addEventListener("click", function(){
+    dec.style.backgroundColor = "red"
+    setTimeout(() => inc.style.backgroundColor = "", 500);
+    a--
+    h1.innerHTML = a
+})
+
+reset.addEventListener("click", function(){
+    reset.style.backgroundColor = "green"
+    setTimeout(() => inc.style.backgroundColor = "", 500);
+    a = 0
+    h1.innerHTML = a
+})
